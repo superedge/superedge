12888d24663ff4134e1fe298cc574030a1c9351b
duyanghao <1294057873@qq.com>
2021-01-12 15:37:13 +0800

    add node controller for DeploymentGridController
    Signed-off-by: duyanghao <1294057873@qq.com>
    Signed-off-by: 00pf00 <feeli202012@gmail.com>

--- pkg/application-grid-controller/controller/deployment/node.go
+++ pkg/application-grid-controller/controller/deployment/node.go
@@ -16,14 +16,93 @@ limitations under the License.
 
 package deployment
 
-func (dgc *DeploymentGridController) addNode(obj interface{}) {
+import (
+	"fmt"
+	crdv1 "github.com/superedge/superedge/pkg/application-grid-controller/apis/superedge.io/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog"
+	"reflect"
+)
 
+func (dgc *DeploymentGridController) addNode(obj interface{}) {
+	node := obj.(*v1.Node)
+	if node.DeletionTimestamp != nil {
+		// On a restart of the controller manager, it's possible for an object to
+		// show up in a state that is already pending deletion.
+		dgc.deleteDeployment(node)
+		return
+	}
+	gs := dgc.getGridForNode(node)
+	if len(gs) == 0 {
+		return
+	}
+	klog.V(4).Infof("Node %s added.", node.Name)
+	for _, g := range gs {
+		dgc.enqueueDeploymentGrid(g)
+	}
 }
 
 func (dgc *DeploymentGridController) updateNode(oldObj, newObj interface{}) {
-
+	oldNode := oldObj.(*v1.Node)
+	curNode := newObj.(*v1.Node)
+	labelChanged := !reflect.DeepEqual(curNode.Labels, oldNode.Labels)
+	// Only handles nodes whose label has changed.
+	if labelChanged {
+		gs := dgc.getGridForNode(curNode)
+		if len(gs) == 0 {
+			return
+		}
+		klog.V(4).Infof("Node %s updated.", curNode.Name)
+		for _, g := range gs {
+			dgc.enqueueDeploymentGrid(g)
+		}
+	}
 }
 
 func (dgc *DeploymentGridController) deleteNode(obj interface{}) {
+	node, ok := obj.(*v1.Node)
+	if !ok {
+		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			utilruntime.HandleError(fmt.Errorf("Couldn't get object from tombstone %#v", obj))
+			return
+		}
+		node, ok = tombstone.Obj.(*v1.Node)
+		if !ok {
+			utilruntime.HandleError(fmt.Errorf("Tombstone contained object is not a node %#v", obj))
+			return
+		}
+	}
+	gs := dgc.getGridForNode(node)
+	if len(gs) == 0 {
+		return
+	}
+	klog.V(4).Infof("Node %s deleted.", node.Name)
+	for _, g := range gs {
+		dgc.enqueueDeploymentGrid(g)
+	}
+}
 
+// getGridForNode get deploymentGrids those gridUniqKey exists in node labels.
+func (dgc *DeploymentGridController) getGridForNode(node *v1.Node) []*crdv1.DeploymentGrid {
+	if len(node.Labels) == 0 {
+		return nil
+	}
+	dgList, err := dgc.dpGridLister.DeploymentGrids("").List(labels.Everything())
+	if err != nil {
+		return nil
+	}
+	var deploymentGrids []*crdv1.DeploymentGrid
+	for _, g := range dgList {
+		if _, exist := node.Labels[g.Spec.GridUniqKey]; exist {
+			deploymentGrids = append(deploymentGrids, g)
+		}
+	}
+	if len(deploymentGrids) == 0 {
+		return nil
+	}
+	return deploymentGrids
 }
