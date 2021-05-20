01544a2849422a30571736968aa09a74275fbb47
00pf00 <feeli202012@gmail.com>
2021-05-20 14:44:44 +0800

    Penetrator add the latest mirror of edgeadm 0.3.0
    Signed-off-by: 00pf00 <feeli202012@gmail.com>

--- build/docker/penetrator-job/build.sh
+++ build/docker/penetrator-job/build.sh
@@ -2,5 +2,5 @@
 
 cp -r pkg/penetrator/job/script "$DST_DIR"
 
-wget https://superedge-1253687700.cos.ap-guangzhou.myqcloud.com/v0.3.0-beta.0/amd64/edgeadm-linux-amd64-v0.3.0-beta.0.tgz -O "$DST_DIR"/edgeadm-amd64-v1.18.2.tar.gz
-wget https://superedge-1253687700.cos.ap-guangzhou.myqcloud.com/v0.3.0-beta.0/amd64/edgeadm-linux-arm64-v0.3.0-beta.0.tgz -O "$DST_DIR"/edgeadm-arm64-v1.18.2.tar.gz
+wget https://attlee-1251707795.cos.ap-chengdu.myqcloud.com/superedge/v0.3.0/edgeadm-linux-amd64-v0.3.0.tgz -O "$DST_DIR"/edgeadm-amd64-v1.18.2.tar.gz
+wget https://attlee-1251707795.cos.ap-chengdu.myqcloud.com/superedge/v0.3.0/edgeadm-linux-arm64-v0.3.0.tgz -O "$DST_DIR"/edgeadm-arm64-v1.18.2.tar.gz
