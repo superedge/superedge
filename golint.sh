2241b0ba5307dfcdeb94e49761590d4d00733746
xiaodongluo <xiaodongluo@tencent.com>
2021-05-21 17:45:18 +0800

    golint update

--- golint.sh
+++ golint.sh
@@ -6,10 +6,12 @@ cd $GOPATH/src/golang.org/x/ && \
 git clone https://github.com/golang/lint.git && \
 cd $GOPATH/src/golang.org/x/lint/golint && \
 go install
-#cd $OLDPATH
-#golint ./... > golint.result
-#count=`cat golint.result|wc -l`
-#if [ $count -gt 0 ]
-#then exit 1
-#else exit 0
-#fi
+cd $OLDPATH
+golint ./... > golint.result
+echo "golint result is:"
+cat golint.result
+count=`cat golint.result|wc -l`
+if [ $count -gt 0 ]
+then exit 1
+else exit 0
+fi
