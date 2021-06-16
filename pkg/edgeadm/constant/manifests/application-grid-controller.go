10a966ded34d8f2abcc9d9fbd39c321a0fa38006
王冬 <wangdong102425@gmail.com>
2021-06-16 20:28:56 +0800

    Version: Update release the v0.4.0.beta.0 version into v0.4.0-beta.0 of SuperEdge (#145)
    Signed-off-by: attleewang <attleewang@tencent.com>
    
    Co-authored-by: attleewang <attleewang@tencent.com>

--- pkg/edgeadm/constant/manifests/application-grid-controller.go
+++ pkg/edgeadm/constant/manifests/application-grid-controller.go
@@ -99,7 +99,7 @@ spec:
       serviceAccountName: application-grid-controller
       containers:
         - name: application-grid-controller
-          image: superedge/application-grid-controller:v0.4.0.beta.0
+          image: superedge/application-grid-controller:v0.4.0-beta.0
           imagePullPolicy: IfNotPresent
           command:
             - /usr/local/bin/application-grid-controller
