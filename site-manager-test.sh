bed1c9ea2ead9da94d802107a25b05c6e3bc58c7
attleewang <attleewang@tencent.com>
2021-11-22 09:43:17 +0800

    site-manager: Updata logs
    Signed-off-by: attleewang <attleewang@tencent.com>

---
+++ site-manager-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make clean
+
+make push IMAGES="site-manager" PLATFORMS="linux_amd64" REGISTRY_PREFIX="ccr.ccs.tencentyun.com/superedge" VERSION="v1"
+
+kubectl -n edge-system apply  -f deployment/site-manager.yaml
+
+kubectl -n edge-system delete pod --all
+
