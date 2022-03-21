c4cda1e8278ae52900a1b094eb7953adcf773d5a
attleewang <attleewang@tencent.com>
2022-03-21 10:24:25 +0800

    site-manager: update node_unit file name
    Signed-off-by: attleewang <attleewang@tencent.com>

--- pkg/site-manager/utils/node_unit.go
+++ pkg/site-manager/utils/node_unit.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/superedge/superedge/pkg/util"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -50,7 +51,31 @@ func GetNodeUitReadyRate(nodeUnit *sitev1.NodeUnit) string {
 	return fmt.Sprintf("%d/%d", len(unitStatus.ReadyNodes), len(unitStatus.ReadyNodes)+len(unitStatus.NotReadyNodes))
 }
 
+func RemoveUnitSetNode(crdClient *crdClientset.Clientset, units, keys []string) error {
+	if len(units) == 0 {
+		return nil
+	}
+	for _, unit := range units {
+		nodeUnit, err := crdClient.SiteV1alpha1().NodeUnits().Get(context.TODO(), unit, metav1.GetOptions{})
+		if err != nil {
+			klog.Errorf("Remove unit setNode get nodeUnit error: %#v", err)
+			continue
+		}
+		setNode := &nodeUnit.Spec.SetNode
+		for _, key := range keys {
+			delete(setNode.Labels, key)
+		}
+		_, err = crdClient.SiteV1alpha1().NodeUnits().Update(context.TODO(), nodeUnit, metav1.UpdateOptions{})
+		if err != nil {
+			klog.Errorf("Remove unit setNode update nodeUnit error: %#v", err)
+			continue
+		}
+	}
+	return nil
+}
+
 func RemoveSetNode(kubeClient clientset.Interface, nodeUnit *sitev1.NodeUnit, nodes []string) error {
+	klog.V(4).Infof("Remove setNode nodeUnit: %s will remove nodes: %s setNode: %s", nodeUnit.Name, nodes, util.ToJson(nodeUnit.Spec.SetNode))
 	for _, nodeName := range nodes {
 		node, err := kubeClient.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
 		if err != nil {
@@ -58,7 +83,7 @@ func RemoveSetNode(kubeClient clientset.Interface, nodeUnit *sitev1.NodeUnit, no
 			continue
 		}
 
-		setNode := nodeUnit.Spec.SetNode
+		setNode := &nodeUnit.Spec.SetNode
 		if setNode.Labels != nil && node.Labels != nil {
 			for labelKey, _ := range setNode.Labels {
 				if _, ok := node.Labels[labelKey]; ok {
